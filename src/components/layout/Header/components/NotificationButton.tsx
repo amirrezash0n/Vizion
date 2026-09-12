49682bf08cbdaebe13b6adee01c99758fd2a5535
amirrezash0n <shourvarziamirreza@gmail.com>
2026-09-12 22:17:31 +0330

    feat(header): show notification dot only when unread notifications exist

--- src/components/layout/Header/components/NotificationButton.tsx
+++ src/components/layout/Header/components/NotificationButton.tsx
@@ -1,12 +1,17 @@
 import { FaRegBell } from "react-icons/fa";
 import { Link } from "react-router";
 import Button from "../../../common/Button/Button";
+import { useNotificationStore } from "../../../../store/notificationStore";
 
 function NotificationButton() {
+  const { notifications } = useNotificationStore();
+  const hasUnread = notifications.some((n) => !n.isRead);
   return (
     <Link to="/notifications">
       <Button className="relative" variant="icon" size="icon">
-        <div className="absolute size-2 top-2.5 right-2.5 bg-primary rounded-full"></div>
+        {hasUnread && (
+          <div className="absolute size-2 top-2.5 right-2.5 bg-primary rounded-full animate-pulse" />
+        )}
         <FaRegBell size={20} />
       </Button>
     </Link>
